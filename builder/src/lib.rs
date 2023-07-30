0cd231031544204ff879c67537ae079cb3992f08
dinhSKM <dinh.le@skymavis.com>
2023-07-30 23:14:14 +0700

    generic all repeative quote with fileds

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -11,48 +11,65 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let bname = format!("{}Builder", name);
     let bident = syn::Ident::new(&bname, name.span());
     eprintln!("bident: {:?}", bident);
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = ast.data
+    {
+        named
+    } else {
+        unimplemented!()
+    };
+    let optionzed = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            #name: Option<#ty>,
+        }
+    });
+
+    let methods = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        }
+    });
+
+    let init_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! { #name: None }
+    });
+
     let expanded = quote! {
         pub struct #bident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#optionzed)*
 
         }
         impl #name {
             fn builder() -> #bident {
                 #bident {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#init_fields,)*
                 }
             }
         }
 
         impl #bident {
-            fn executable(&mut self, executable: String) -> &mut Self {
-                self.executable = Some(executable);
-                self
-            }
-            fn args(&mut self, args: Vec<String>) -> &mut Self {
-                self.args = Some(args);
-                self
-            }
-            fn env(&mut self, env: Vec<String>) -> &mut Self {
-                self.env = Some(env);
-                self
-            }
-            fn current_dir(&mut self, current_dir: String) -> &mut Self {
-                self.current_dir = Some(current_dir);
-                self
-            }
+            #(#methods)*
+
             fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
                 Ok(#name {
-                    executable: self.executable.clone().ok_or("executable is not set")?,
-                    args: self.args.clone().ok_or("args is not set")?,
-                    env: self.env.clone().ok_or("env is not set")?,
-                    current_dir: self.current_dir.clone().ok_or("current_dir is not set")?,
+                    #(#build_fields,)*
                 })
             }
         }
--- main.rs
+++ main.rs
@@ -1,9 +1,24 @@
-// Write code here.
-//
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
-//
-// To run the code:
-//     $ cargo run
+// This test case should be a freebie if the previous ones are already working.
+// It shows that we can chain method calls on the builder.
 
-fn main() {}
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .current_dir("..".to_owned())
+        .build()
+        .unwrap();
+
+    assert_eq!(command.executable, "cargo");
+}
